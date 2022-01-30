b497d5c02c6e661737fa79a908297c520c2f2b2c
Juan Fernandez <juanfdx@gmail.com>
2022-01-30 06:52:11 +0100

    thefork website v1.0.3

--- src/app/components/como-funciona/como-funciona.component.ts
+++ src/app/components/como-funciona/como-funciona.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComoFuncionaComponent implements OnInit {
 
+  position    : number = 0
+  imgShow     : number = 1
+  imgAmount   : number = 4 //this.images.length
+  slideMoveX  : number = this.imgAmount / this.imgShow // slideMoveX = 4
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  itemSelected( i : number): void {
+    this.position = i   
+  }
+
+  //ancho del slider
+  slideWidth(): string {
+    return `${ this.slideMoveX * 100 }%` //400%
+  }
+
+  //ancho de cada articulo
+  articleWidth(): string {
+    return `${ 100 / this.imgAmount }%` 
+  }
+
+  //valor de desplazamiento
+  translateXvalue(): string {
+    return `translateX(${ this.position * -( 100 / this.slideMoveX )}%)`
   }
 
 }
